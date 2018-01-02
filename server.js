aa05bf853fd8ddd47d149323c2d644a2937cd971
Evan Baker <3van90@gmail.com>
2018-01-02 18:42:05 -0500

    Start basic server

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+app.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
