6bc057ee18b48e60b268216f7c939ef28b2e93d1
Evan Baker <3van90@gmail.com>
2018-01-04 10:45:52 -0500

    Start setting up routes

--- server.js
+++ server.js
@@ -2,11 +2,36 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyparser = require('body-parser');
+const db = require('./db/helpers.js');
+
+app.use(bodyparser.json());
 
 // Serve up static files
 app.use(express.static(path.join(__dirname, '/client/www')));
 
-// Routes
+// Routes for login/signup
+app.post('/signup', (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.doesUserExist(username, (exists) => {
+    if (exists) {
+      res.send('user already exists');
+    } else {
+      
+    }
+  })
+})
+
+app.post('/user', (req, res) => {
+  const username = req.body.username;
+  db.getUserByName(username, (results) => {
+    res.json(results);
+  });
+})
+
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server listening on Port ${PORT}`));
