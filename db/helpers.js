83b8d22778a59f135cefa066d4b368a52472b07c
Evan Baker <3van90@gmail.com>
2018-01-04 11:15:13 -0500

    Create new server side routes

--- db/helpers.js
+++ db/helpers.js
@@ -1,38 +1,38 @@
 const User = require('./models/user.js')
 const Record = require('./models/records.js')
 
-// const doesUserExist = function(username, cb) {
-//   User.findAll({
-//     where: {
-//       "username": username
-//     }
-//   })
-//   .then(results => {
-//     if (results.length === 0) {
-//       cb()
-//     } else {
-//       console.log('user already exists');
-//     }
-//   })
-//   .catch(err => {
-//     console.log('error:', err)
-//   })
-// }
+const doesUserExist = function(username, cb) {
+  User.findAll({
+    where: {
+      "username": username
+    }
+  })
+  .then(results => {
+    if (results.length === 0) {
+      cb(false)
+    } else {
+      cb(true);
+    }
+  })
+  .catch(err => {
+    console.log('error:', err)
+  })
+}
 
-// const addNewUser = function(userInfo) {
-//   doesUserExist(userInfo.username, () => {
-//     const newUser = User.create({
-//       "username": userInfo.username,
-//       "password": userInfo.password
-//     })
-//       .then(() => {
-//         console.log('success');
-//       })
-//       .catch(err => {
-//         console.log('error: ', err);
-//       })
-//   })
-// }
+const addNewUser = function(userInfo) {
+  doesUserExist(userInfo.username, () => {
+    const newUser = User.create({
+      "username": userInfo.username,
+      "password": userInfo.password
+    })
+      .then(() => {
+        console.log('success');
+      })
+      .catch(err => {
+        console.log('error: ', err);
+      })
+  })
+}
 
 
 
--- server.js
+++ server.js
@@ -11,24 +11,31 @@ app.use(bodyparser.json());
 app.use(express.static(path.join(__dirname, '/client/www')));
 
 // Routes for login/signup
-app.post('/signup', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  db.doesUserExist(username, (exists) => {
+// checks if username exists. if it doesn't it create the user in db
+app.post('/signup', (req, res) => {
+  db.doesUserExist(req.body.username, (exists) => {
     if (exists) {
-      res.send('user already exists');
+      res.json('Username Already Exists');
     } else {
-      
+      db.addNewUser(req.body)
+      res.json('User Account Created');
     }
   })
 })
 
+// returns all information about user that exists in database
 app.post('/user', (req, res) => {
   const username = req.body.username;
   db.getUserByName(username, (results) => {
     res.json(results);
   });
+})
+
+// save new record to database
+app.post('newRecord', (req, res) => {
+  db.addNewRecord(req.body);
+  res.send('Record Added to Database');
 })
 
 
