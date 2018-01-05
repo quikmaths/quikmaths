1f2cfc7b9b9054a470cd7f9184e1c765fa83e5e4
Evan Baker <3van90@gmail.com>
2018-01-05 14:51:31 -0500

    Add styles for login/signup

--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -1,6 +1,60 @@
 import React from 'react'
 import axios from 'axios'
 
+const containerStyle = {
+  display: "grid",
+  height: "100vh",
+  gridTemplateColumns: "15% 40% 30% 15%",
+  gridTemplateRows: "20% 40% 10% 10% 20%",
+  backgroundColor: "#618985",
+  fontFamily: "poppins"
+}
+
+const introStyle = {
+  gridColumnStart: "2",
+  gridColumnEnd: "3",
+  gridRowStart: "2",
+  gridRowEnd: "3",
+  justifySelf: "left",
+  alignSelf: "top",
+}
+
+const signUpStyle = {
+  gridColumnStart: "3",
+  gridColumnEnd: "4",
+  gridRowStart: "2",
+  gridRowEnd: "3",
+  justifySelf: "center"
+}
+
+const signUpButtonStyle = {
+  width: "100%",
+}
+
+const loginStyle = {
+  gridColumnStart: "2",
+  gridColumnEnd: "4",
+  gridRowStart: "3",
+  gridRowEnd: "4",
+  justifySelf: "center",
+  alignSelf: "top",
+  marginBottom: "5px"
+}
+
+const loginButtonStyle = {
+  width: "100%",
+}
+
+const smallPrintStyle = {
+  gridColumnStart: "2",
+  gridColumnEnd: "4",
+  gridRowStart: "4",
+  gridRowEnd: "5",
+  fontSize: "10px",
+  justifySelf: "center",
+  marginTop: "50px"
+}
+
 class SignUp extends React.Component{
 	constructor(props){
 		super(props)
@@ -27,12 +81,23 @@ class SignUp extends React.Component{
 
 	render(){
 		return(
-			<div>
-				<h1>Sign Up!</h1>
-				<p>Username: <input type="text" value={this.state.username} onChange={this.handleUsername}></input></p>
-				<p>Password: <input type="password" value={this.state.password} onChange={this.handlePassword}></input></p>
-				<button onClick={() => this.props.handleSignUp({'username': this.state.username, 'password': this.state.password})}>Sign Up</button>
-				<button onClick={this.props.goToLogin}>I have an account!</button>
+			<div style={containerStyle}>
+				<div style={introStyle}>
+					<h1>Welcome to QuikMath</h1>
+          <h4>Learn, Play, Compete</h4>
+          <p>It's like HQ trivia but it's only for math, you can't win money, and we have a tenth of a percentage of their userbase</p>
+				</div>
+				<div style={signUpStyle}>
+					<h1>Sign Up!</h1>
+					<p>Username: <input type="text" value={this.state.username} onChange={this.handleUsername}></input></p>
+					<p>Password: <input type="password" value={this.state.password} onChange={this.handlePassword}></input></p>
+					<button style={signUpButtonStyle} onClick={() => this.props.handleSignUp({'username': this.state.username, 'password': this.state.password})}>Sign Up</button>
+				</div>
+				<div style={loginStyle}>
+					<p>I Alredy Have An Account</p>
+					<button style={loginButtonStyle} onClick={this.props.goToLogin}>I have an account!</button>
+				</div>
+				<p style={smallPrintStyle}>&copy; 2018 QuikMath Fine Print Goes Here</p>
 			</div>
 		)
 	}
